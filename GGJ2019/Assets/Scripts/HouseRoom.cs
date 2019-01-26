f2fe6bfb5adc9b67376912e416c42ca1dadfb996
Unknown <juanjvalenciagd@gmail.com>
2019-01-26 00:47:13 -0800

    Added first pass House UI and button callbacks.

---
+++ GGJ2019/Assets/Scripts/HouseRoom.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HouseRoom : MonoBehaviour
+{
+    // Start is called before the first frame update
+   FamilyMember[] charactersInRoom;
+   public string roomSceneName;
+   public string[] roomActions;
+
+   private void Awake() {
+       GetComponent<Button>().onClick.AddListener(ButtonAction);
+   }
+
+   public void ButtonAction(){
+       RoomCanvasManager.instance.ShowRoom(roomSceneName, roomActions);
+   }
+}
---
+++ GGJ2019/Assets/Scripts/RoomCanvasManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomCanvasManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text RoomNameText;
+    public static RoomCanvasManager instance;
+    CanvasGroup roomCanvas;
+    public GameObject[] roomButtons;
+    public Text[] buttonText;
+
+    private void Awake() {
+
+        if(instance != null){
+            Destroy(this);
+        }
+        instance = this;
+        roomCanvas = GetComponent<CanvasGroup>();
+
+    }
+
+    public void ShowRoom(string roomName, string[] actions){
+
+        for(int i = 0; i < actions.Length; i++){
+
+            roomButtons[i].SetActive(true);
+            buttonText[i].text=actions[i];
+            roomButtons[i].GetComponent<RoomOption>().buttonID = i;
+            
+
+        }
+
+        HouseManager.instance.HideHouse();
+        RoomNameText.text = roomName;
+        roomCanvas.interactable = true;
+        roomCanvas.blocksRaycasts = true;
+        roomCanvas.alpha = 1;
+
+    }
+
+    public void HideRoom(){
+        roomCanvas.interactable = false;
+        roomCanvas.blocksRaycasts = false;
+        roomCanvas.alpha = 0;
+        for(int i = 0; i < roomButtons.Length; i++){
+
+
+            roomButtons[i].gameObject.SetActive(false);
+
+        }
+    }
+
+    public void OnBackButtonClicked(){
+        HouseManager.instance.ShowHouse();
+        HideRoom();
+    }
+    
+}
