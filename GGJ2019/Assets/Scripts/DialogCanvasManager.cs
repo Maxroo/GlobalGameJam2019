8b0d83db930e3476d1d59fb805b3f0b6f518d5d0
Unknown <juanjvalenciagd@gmail.com>
2019-01-26 15:51:13 -0800

    Added Test Dialog Scene and event Dialog Implementation.

---
+++ GGJ2019/Assets/Scripts/DialogCanvasManager.cs
@@ -0,0 +1,205 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogCanvasManager : MonoBehaviour
+{
+    public static DialogCanvasManager instance;
+
+    public enum CharactersToShow{
+        MOM,
+        DAD,
+        SISTER,
+        BROTHER,
+        DOG,
+        NONE
+    }
+
+    [Header("Buttons")]
+    public Button FirstOption;
+    public Button SecondOption;
+
+    [Header("Images")]
+   public Image leftDialogBox;
+   public Image rightDialogBox;
+   public Image dialogBackground;
+
+   [Header("Text")]
+    public Text leftDialogName;
+   public Text rightDialogName;
+   public Text dialogText;
+   public Text firstButtonText;
+
+   [Header("CharacterSprites")]
+   public Image momSprite;
+   public Image dadSprite;
+   public Image sisSprite;
+   public Image broSprite;
+
+   private Image leftSprite;
+   private Image rightSprite;
+
+   private void Awake() {
+
+       if(instance != null){
+           instance = this;
+       }
+       instance = this;
+
+   }
+
+   public void ShowLeftDialogBox(CharactersToShow character){
+       leftDialogBox.gameObject.SetActive(true);
+
+       string nameString = null;
+       switch(character){
+
+           case DialogCanvasManager.CharactersToShow.MOM:
+           nameString = "Mom";
+           break;
+
+           case DialogCanvasManager.CharactersToShow.DAD:
+           nameString = "Dad";
+           break;
+
+           case DialogCanvasManager.CharactersToShow.SISTER:
+           nameString = "Sister";
+           break;
+
+           case DialogCanvasManager.CharactersToShow.BROTHER:
+           nameString = "Brother";
+           break;
+
+       }
+
+       leftDialogName.text = nameString;
+   }
+
+   public void ShowRightDialogBox(CharactersToShow character){
+       rightDialogBox.gameObject.SetActive(true);
+
+        string nameString = null;
+
+       switch(character){
+           case DialogCanvasManager.CharactersToShow.MOM:
+           nameString = "Mom";
+           break;
+
+           case DialogCanvasManager.CharactersToShow.DAD:
+           nameString = "Dad";
+           break;
+
+           case DialogCanvasManager.CharactersToShow.SISTER:
+           nameString = "Sister";
+           break;
+
+           case DialogCanvasManager.CharactersToShow.BROTHER:
+           nameString = "Brother";
+           break;
+       }
+       
+       rightDialogName.text = nameString;
+   }
+
+   public void HideRightDialog(){
+       rightDialogBox.gameObject.SetActive(false);
+   }
+
+   public void HideLeftDialog(){
+       leftDialogBox.gameObject.SetActive(false);
+   }
+
+   public void HideDialogNames(){
+        rightDialogBox.gameObject.SetActive(false);
+        leftDialogBox.gameObject.SetActive(false);
+   }
+
+   public void ShowEventDialog(string dialogString){
+       dialogBackground.gameObject.SetActive(true);
+       dialogText.text = dialogString;
+   }
+
+   public void ShowCharacterLeft(CharactersToShow character){
+
+       Image spriteToShow = null;
+
+       switch(character){
+           case DialogCanvasManager.CharactersToShow.MOM:
+           spriteToShow = momSprite;
+           break;
+
+           case DialogCanvasManager.CharactersToShow.DAD:
+           spriteToShow = dadSprite;
+           break;
+
+           case DialogCanvasManager.CharactersToShow.SISTER:
+           spriteToShow = sisSprite;
+           break;
+
+           case DialogCanvasManager.CharactersToShow.BROTHER:
+           spriteToShow = broSprite;
+           break;
+       }
+
+       if(spriteToShow != null){
+
+            spriteToShow.enabled = true;
+            spriteToShow.rectTransform.localScale= new Vector3(-1, 1, 1);
+            leftSprite = spriteToShow;
+            leftSprite.gameObject.GetComponent<SpriteMovement>().MoveInLeft();
+
+       }
+
+   }
+
+
+
+    public void ShowCharacterRight(CharactersToShow character){
+
+       Image spriteToShow = null;
+
+       switch(character){
+           case DialogCanvasManager.CharactersToShow.MOM:
+           spriteToShow = momSprite;
+           break;
+
+           case DialogCanvasManager.CharactersToShow.DAD:
+           spriteToShow = dadSprite;
+           break;
+
+           case DialogCanvasManager.CharactersToShow.SISTER:
+           spriteToShow = sisSprite;
+           break;
+
+           case DialogCanvasManager.CharactersToShow.BROTHER:
+           spriteToShow = broSprite;
+           break;
+       }
+        if(spriteToShow != null){
+            spriteToShow.enabled = true;
+            spriteToShow.rectTransform.localScale= new Vector3(-1, 1, 1);
+            rightSprite = spriteToShow;
+            rightSprite.gameObject.GetComponent<SpriteMovement>().MoveInRight();
+
+        }
+   }
+
+  
+
+   public void HideCharacterLeft(){
+       leftSprite.gameObject.GetComponent<SpriteMovement>().MoveOutLeft();
+
+   }
+
+    public void HideCharacterRight(){
+        if(rightSprite != null){
+
+            rightSprite.gameObject.GetComponent<SpriteMovement>().MoveOutRight();
+        }
+
+    }
+
+ 
+
+}
---
+++ GGJ2019/Assets/Scripts/DialogEvent.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogEvent : MonoBehaviour
+{
+
+    private bool spriteOutOnLeft;
+    private bool spriteOutOnRight;
+
+    private DialogCanvasManager.CharactersToShow characterOnRight;
+    private DialogCanvasManager.CharactersToShow characterOnLeft;
+
+    public EventAction[] eventArray;
+
+    private int actionNumber = 0;
+
+    public void NextAction(){
+
+        DialogCanvasManager.instance.dialogBackground.gameObject.SetActive(true);
+
+        EventAction currentAction = eventArray[actionNumber];
+
+        if(currentAction.LeftCharacter != DialogCanvasManager.CharactersToShow.NONE){
+
+            if(!spriteOutOnLeft){
+                DialogCanvasManager.instance.ShowCharacterLeft(currentAction.LeftCharacter);
+                DialogCanvasManager.instance.ShowLeftDialogBox(currentAction.LeftCharacter);
+                characterOnLeft = currentAction.LeftCharacter;
+                spriteOutOnLeft = true;
+            } else if(characterOnLeft != currentAction.LeftCharacter){
+                DialogCanvasManager.instance.HideCharacterLeft();
+                DialogCanvasManager.instance.ShowCharacterLeft(currentAction.LeftCharacter);
+                DialogCanvasManager.instance.ShowLeftDialogBox(currentAction.LeftCharacter);
+                characterOnLeft = currentAction.LeftCharacter;
+                spriteOutOnLeft = true;
+            }
+        } else if (currentAction.LeftCharacter == DialogCanvasManager.CharactersToShow.NONE){
+            if(spriteOutOnLeft){
+                DialogCanvasManager.instance.HideCharacterLeft();
+                DialogCanvasManager.instance.HideLeftDialog();
+                spriteOutOnLeft = false;
+                characterOnLeft = DialogCanvasManager.CharactersToShow.NONE;
+            }
+
+        }
+
+        if(currentAction.RightCharacter != DialogCanvasManager.CharactersToShow.NONE){
+
+            if(!spriteOutOnRight){
+
+                DialogCanvasManager.instance.ShowCharacterRight(currentAction.RightCharacter);
+                DialogCanvasManager.instance.ShowRightDialogBox(currentAction.RightCharacter);
+                characterOnRight = currentAction.LeftCharacter;
+                spriteOutOnRight = true;
+
+            } else if(characterOnRight != currentAction.RightCharacter){
+
+                DialogCanvasManager.instance.HideCharacterRight();
+                DialogCanvasManager.instance.ShowCharacterRight(currentAction.RightCharacter);
+                DialogCanvasManager.instance.ShowRightDialogBox(currentAction.RightCharacter);
+                characterOnLeft = currentAction.LeftCharacter;
+                spriteOutOnRight = true;
+
+            } 
+            
+        } else{
+            DialogCanvasManager.instance.HideCharacterRight();
+            DialogCanvasManager.instance.HideRightDialog();
+            spriteOutOnRight = false;
+            characterOnRight = DialogCanvasManager.CharactersToShow.NONE;
+        }
+
+        if(currentAction.LeftCharacter == DialogCanvasManager.CharactersToShow.NONE && currentAction.RightCharacter == DialogCanvasManager.CharactersToShow.NONE){
+            DialogCanvasManager.instance.HideRightDialog();
+            DialogCanvasManager.instance.HideLeftDialog();
+
+            if(characterOnRight != DialogCanvasManager.CharactersToShow.NONE){
+                DialogCanvasManager.instance.HideCharacterRight();
+            }
+
+            if(characterOnLeft != DialogCanvasManager.CharactersToShow.NONE){
+                DialogCanvasManager.instance.HideCharacterLeft();
+            }
+
+
+        }
+
+        if(spriteOutOnRight && currentAction.showLeftName){
+            DialogCanvasManager.instance.ShowLeftDialogBox(currentAction.LeftCharacter);
+        } else if(spriteOutOnRight && currentAction.showRightName){
+            DialogCanvasManager.instance.ShowRightDialogBox(currentAction.RightCharacter);
+        }
+            
+        DialogCanvasManager.instance.ShowEventDialog(currentAction.dialogText);
+        actionNumber++;
+
+    }
+
+    private void Update() {
+        if(Input.GetMouseButtonUp(0) && actionNumber < eventArray.Length){
+            NextAction();
+        }
+    }
+
+
+}
