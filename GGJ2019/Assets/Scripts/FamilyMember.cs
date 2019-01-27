2d82066d74a730264823920ce7a800450b458631
Diego <drofferni@gmail.com>
2019-01-26 16:53:12 -0800

    status bar and some character status

--- GGJ2019/Assets/Scripts/FamilyMember.cs
+++ GGJ2019/Assets/Scripts/FamilyMember.cs
@@ -1,22 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FamilyMember : MonoBehaviour
 {
-    
+
     //Stats
-    public string characterName;
-    public int mood;
-    public int loyalty;
-    public int sanity;
-    public int relationship;
+    [SerializeField] string characterName;
+    [Range(0,1)][SerializeField] float mood = 0.9f;
+    [Range(0, 1)] [SerializeField] float loyalty = 1f;
+    [Range(0, 1)] [SerializeField] float relationship = 0.25f;
+    private Slider[] stats;
 
     //Status Variables
-    bool isRobot;
-    bool hasLeft;
-    bool isDead;
+    private bool isRobot;
+    private bool hasLeft;
+    private bool isDead;
 
+    private void Start() {
+        stats = GetComponentsInChildren<Slider>();
+        ChangeStats(mood, loyalty, relationship);
+    }
 
+    public void ChangeStats(float mood, float loyalty, float relationship) {
+        foreach (var stat in stats) {
+            if (stat.name == "Mood") {
 
+                stat.value = mood;
+
+            }
+            if (stat.name == "Loyalty") {
+
+                stat.value = loyalty;
+
+            }
+            if (stat.name == "Relationship") {
+
+                stat.value = relationship;
+
+            }
+        }
+    }
+
+    private void Update() {
+        ChangeStats(mood, loyalty, relationship);
+    }
 }
---
+++ GGJ2019/Assets/Scripts/FamilyStats.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FamilyStats : MonoBehaviour {
+
+    private bool toggleStatsButton;
+    [SerializeField] GameObject[] path;
+    private int moveSpeed = 1000;
+
+    public void ToggleFamilyStats() {
+
+        toggleStatsButton = !toggleStatsButton;
+
+        if (toggleStatsButton) {
+            ShowFamilyStats();
+
+        } else {
+            HideFamilyStats();
+        }
+    }
+
+    public void ShowFamilyStats() {
+        transform.position = Vector2.MoveTowards(
+            transform.position,
+            path[1].transform.position,
+            moveSpeed
+            );
+    }
+
+    public void HideFamilyStats() {
+    transform.position = Vector2.MoveTowards(
+        transform.position,
+        path[0].transform.position,
+        moveSpeed
+        );
+    }
+
+    
+}
