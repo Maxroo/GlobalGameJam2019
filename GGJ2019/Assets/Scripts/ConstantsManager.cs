cc46246d9ca2329dcd938d494b15896252f1ad22
CS <hecklerofharbingers@gmail.com>
2019-01-26 16:42:32 -0800

    Added game clock, test scene

---
+++ GGJ2019/Assets/Scripts/ConstantsManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ConstantsManager
+{
+    public const int kBuffer = 10;
+    public const int kLeaveThreshold = 10;
+    public const int kFightThreshold = 40;
+    public const int kArgueThreshold = 50;
+    //Chances of events
+    public const int kChanceMinorEvent = 40;
+    public const int kChanceMajorEvent = 40;
+    //intervals
+    public const int kSmallInterval = 5;
+    public const int kMediumInterval = 10;
+    public const int kLargeInterval = 20;
+
+}
---
+++ GGJ2019/Assets/Scripts/GameClock.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameClock : MonoBehaviour
+{
+	public Text timeText;
+	public Text dayText;
+    public int startTime = 8;
+    public int numberOfDays = 4;
+
+   private TimeBlock[] dayCycle = {
+        new TimeBlock(8,TimeOfDay.Morning, 2),
+        new TimeBlock(10, TimeOfDay.Morning, 2),
+        new TimeBlock(12, TimeOfDay.Afternoon, 2),
+        new TimeBlock(14, TimeOfDay.Afternoon, 2),
+        new TimeBlock(16, TimeOfDay.Afternoon, 2),
+        new TimeBlock(18, TimeOfDay.Evening, 2),
+        new TimeBlock(20, TimeOfDay.Evening, 2),
+        new TimeBlock(22, TimeOfDay.Evening, 2),
+        new TimeBlock(24, TimeOfDay.Evening, 8)
+    };
+
+    private TimeOfDay lastTOD = TimeOfDay.Morning;
+	private int currentTimeBlock = 0;
+	private int currentTime = 8;
+
+    //--------------------------------------------------Unity Methods--------------------------------------------------
+    void Awake()
+    {
+        //On game 
+        restartClock();
+        
+    }
+
+ 
+    void Update()
+    {
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			GoToNextSegment ();
+		}
+    }
+
+    //--------------------------------------------------Public Methods--------------------------------------------------
+
+    public bool GoToNextSegment() {
+        //go to next segment
+        currentTime += dayCycle[currentTimeBlock].Hours;
+        lastTOD = dayCycle[currentTimeBlock].TOD;
+        currentTimeBlock = GetNextIndexInSequence(currentTimeBlock);
+        //call event if chance rolls
+        if (lastTOD!= dayCycle[currentTimeBlock].TOD)
+        {
+            //roll for major event
+            if (Random.Range(0, 100) > ConstantsManager.kChanceMajorEvent)
+            {
+                //major event
+            }
+        }
+        else
+        {
+            if (Random.Range(0, 100) > ConstantsManager.kChanceMinorEvent)
+            {
+                //minor event
+            }
+
+        }
+
+        //Update UI
+        timeText.text = GetCurrentTimeString();
+		dayText.text = GetCurrentDayString ();
+        //return if is out of time
+        return (currentTime >= (numberOfDays*24));   
+    }
+
+	public string GetCurrentDayString(){
+		string rtn = "Day " + (Mathf.FloorToInt(currentTime/24)+1);
+//		Debug.Log (rtn);
+		return rtn;
+	}
+
+    public string GetCurrentTimeString()
+    {
+        string rtn = "";
+        bool isAM = (currentTime % 24) < 12;
+        int hourOfDay = currentTime % 12;
+        if (hourOfDay == 0) { hourOfDay = 12; }
+        rtn += hourOfDay + ":00" + (isAM ? "am" : "pm");
+        if (hourOfDay < 10) { rtn = " " + rtn; }
+        return rtn;
+    }
+    //--------------------------------------------------Private--------------------------------------------------
+    private int GetNextIndexInSequence(int i)
+    {
+        int rtn = i+1;
+        if (i >= dayCycle.Length)
+        {
+            rtn = 0;
+        }
+        return rtn;
+    }
+
+    private void restartClock()
+    {
+        currentTime = startTime;
+        SetCurrentTimeBlock();
+        
+        Debug.Log("Clock restarted: " + GetCurrentTimeString());
+    }
+
+    private void SetCurrentTimeBlock()
+    {
+        int timeFound = -1;
+        int tempTime = currentTime % 24;
+        for (int i = 0; i < dayCycle.Length; ++i)
+        {
+            if (dayCycle[i].time == tempTime)
+            {
+                timeFound = i;
+                break;
+            }
+        }
+        if (timeFound == -1)
+        {
+            Debug.Log("Time does not exist");
+            timeFound = 0;
+        }
+        currentTimeBlock = timeFound;
+
+    }
+
+}
+public class TimeBlock
+{
+    public int time = 0;
+    public int Hours = 2;
+    public TimeOfDay TOD = TimeOfDay.Morning;
+    public TimeBlock(int t, TimeOfDay tod, int hrs) { TOD = tod; time = t; Hours = hrs; }
+}
+
+public enum TimeOfDay { Morning, Afternoon, Evening, Night };
+
