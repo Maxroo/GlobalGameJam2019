5d2ed4e933566d147a2006d45fb1f104536ebf4d
Unknown <juanjvalenciagd@gmail.com>
2019-01-27 15:21:25 -0800

    Making Character Image Display

---
+++ GGJ2019/Assets/Scripts/CharacterImageDisplay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterImageDisplay : MonoBehaviour
+{
+
+    public Room roomToCheck;
+    public Image[] characterSprites;
+
+    public void checkCharactersInRoom(){
+
+        FamilyCharacter[] chars = CharacterManager.instance.GetCharacters();
+       for(int i = 0; i < chars.Length; i++){
+           
+           if(chars[i].currentRoom == roomToCheck){
+               characterSprites[i].enabled = true;
+           } else{
+               characterSprites[i].enabled = false;
+           }
+       }
+
+    }
+
+
+}
